571dabe09c42d0844eb055583e71103bbcab48e6
asivath <asivatha@purdue.edu>
2024-09-12 16:44:14 -0400

    adjusted index test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -89,7 +89,7 @@ describe("E2E Test", () => {
       // Don't get the values from ./run test (would defeat the purpose of this test), run the tests using npm run test and get the values from there and get coverage from npm run test:coverage
       expect(totalTests).toBe(21);
       expect(totalPassed).toBe(21);
-      expect(lineCoverage).toBe(93.47);
+      expect(lineCoverage).toBe(93.88);
     }
   });
 
